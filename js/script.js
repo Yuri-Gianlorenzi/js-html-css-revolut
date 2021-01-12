01b0b926383125400b297486ffb9e1402becaaba
Yuri Gianlorenzi <yurigian@gmail.com>
2021-01-12 22:41:15 +0100

    add reverse chevron and color text black on hover

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
 
+  var turnChevron = $('ul.nav_list li:nth-last-child(2) a');
+  console.log(turnChevron);
   $('.nav_list li').hover(function(){
     // $('.dropdown-menu').toggleClass('active');
 
+
     //ipotesi più dropdown
     $(this).children('ul').toggleClass('active');
+    $(this).children('a').toggleClass('t_black');
+
+    // con questo if, riusciamo a capovolgere la chevron una volta che ci passiamo sopra, per poi farla tornare alla posizione iniziale una volta che andiamo via dall'hover
+    if (turnChevron.hasClass('t_black')){
+      $('ul.nav_list li:nth-last-child(2) span i:first-child').addClass('d_none');
+      $('ul.nav_list li:nth-last-child(2) span i:last-child').removeClass('d_none');
+    } else {
+      $('ul.nav_list li:nth-last-child(2) span i:first-child').removeClass('d_none');
+      $('ul.nav_list li:nth-last-child(2) span i:last-child').addClass('d_none');
+    }
+
   });
 
 
